26b15befc4bc1b762c312b3f02c261ad0fe07313
manupanand <manupanandindianinvestor@gmail.com>
2025-01-24 23:11:45 +0530

    test deploy

--- service/common/error_handlers.py
+++ service/common/error_handlers.py
@@ -1,46 +1,66 @@
 """
 Module: error_handlers
 """
+
 from flask import jsonify
 from service import app
 from . import status
 
-# General Error Handler
+
 def handle_error(error, status_code, error_message):
-    """Handles all error types"""
+    """Handles all error types."""
     app.logger.warning(f"{error_message}: {str(error)}")
-    return jsonify(
-        status=status_code, error=error_message, message=str(error)
-    ), status_code
+    return (
+        jsonify(
+            status=status_code,
+            error=error_message,
+            message=str(error),
+        ),
+        status_code,
+    )
+
 
 # Error Handlers for Specific HTTP Status Codes
 @app.errorhandler(status.HTTP_400_BAD_REQUEST)
 def bad_request(error):
-    """Handles bad requests (400)"""
+    """Handles bad requests (400)."""
     return handle_error(error, status.HTTP_400_BAD_REQUEST, "Bad Request")
+
 
 @app.errorhandler(status.HTTP_404_NOT_FOUND)
 def not_found(error):
-    """Handles not found errors (404)"""
+    """Handles not found errors (404)."""
     return handle_error(error, status.HTTP_404_NOT_FOUND, "Not Found")
+
 
 @app.errorhandler(status.HTTP_405_METHOD_NOT_ALLOWED)
 def method_not_supported(error):
-    """Handles method not allowed errors (405)"""
+    """Handles method not allowed errors (405)."""
     return handle_error(error, status.HTTP_405_METHOD_NOT_ALLOWED, "Method Not Allowed")
+
 
 @app.errorhandler(status.HTTP_409_CONFLICT)
 def resource_conflict(error):
-    """Handles conflict errors (409)"""
+    """Handles conflict errors (409)."""
     return handle_error(error, status.HTTP_409_CONFLICT, "Conflict")
+
 
 @app.errorhandler(status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 def mediatype_not_supported(error):
-    """Handles unsupported media type errors (415)"""
-    return handle_error(error, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE, "Unsupported Media Type")
+    """Handles unsupported media type errors (415)."""
+    return handle_error(
+        error,
+        status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+        "Unsupported Media Type",
+    )
+
 
 @app.errorhandler(status.HTTP_500_INTERNAL_SERVER_ERROR)
 def internal_server_error(error):
-    """Handles internal server errors (500)"""
+    """Handles internal server errors (500)."""
     app.logger.error(f"Internal Server Error: {str(error)}")
-    return handle_error(error, status.HTTP_500_INTERNAL_SERVER_ERROR, "Internal Server Error")
+    return handle_error(
+        error,
+        status.HTTP_500_INTERNAL_SERVER_ERROR,
+        "Internal Server Error",
+    )
--- service/common/log_handlers.py
+++ service/common/log_handlers.py
@@ -5,8 +5,9 @@ This module contains utility functions to set up logging consistently.
 """
 import logging
 
+
 def init_logging(app, logger_name: str):
-    """Set up logging for production"""
+    """Set up logging for production."""
     app.logger.propagate = False
     gunicorn_logger = logging.getLogger(logger_name)
     app.logger.handlers = gunicorn_logger.handlers
@@ -14,9 +15,10 @@ def init_logging(app, logger_name: str):
 
     # Make all log formats consistent
     formatter = logging.Formatter(
-        "[%(asctime)s] [%(levelname)s] [%(module)s] %(message)s", "%Y-%m-%d %H:%M:%S %z"
+        "[%(asctime)s] [%(levelname)s] [%(module)s] %(message)s",
+        "%Y-%m-%d %H:%M:%S %z",
     )
     for handler in app.logger.handlers:
         handler.setFormatter(formatter)
-    
+
     app.logger.info("Logging handler established")
